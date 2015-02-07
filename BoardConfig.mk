596d96b32b8dbafd72e9d253094f5a6cb69654ee
Dominique DAMBRAIN <domi@dambrain.fr>
2015-02-07 10:03:03 +0100

    Update BoardConfig.mk
    fix typo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,9 +43,10 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 ARCH_ARM_HIGH_OPTIMIZATION := true
 
-<!-- Treat the on screen buttons as hardware keys?
-     suppressed for Lollipop
+<!-- Treat the on screen buttons as hardware keys -->
+     <!-- suppressed for Lollipop 
     <integer name="config_deviceHardwareKeys">7</integer>  -->
+    
 # Change to test tf101 config
 TARGET_OTA_ASSERT_DEVICE := picasso_e,a200
 # Change to test tf101 config
