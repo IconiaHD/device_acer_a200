95bfe6f1e340a5345949add1ed3e6605ec918898
Dominique DAMBRAIN <domi@dambrain.fr>
2015-02-12 15:27:14 +0100

    Update a200.mk

--- a200.mk
+++ a200.mk
@@ -21,17 +21,37 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/zImage:kernel
 
 # Set wifi-only before it's set by generic_no_telephony.mk
+WIFI_BAND := 802_11_ABG
+
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carrier=wifi-only
+    ro.carrier=wifi-only \
+    ro.boot.selinux=disabled \
+    ro.build.selinux=0 \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=30 \
+    ro.dinfo.radio=Wifi \
+    ro.wifi.country=EU \
+    ro.zygote.disable_gl_preload=true \
+    ro.bq.gpu_to_cpu_unsupported=1 \
+    debug.hwui.render_dirty_regions=false \
+    ro.opengles.version=131072 \
+    ro.opengles.surface.rgb565=true \
+    ro.hwui.use_gpu_pixel_buffers=false
+
+PRODUCT_PACKAGES += \
+    libnetcmdiface \
+    libwpa_client \
+    hostapd \
+    dhcpcd.conf \
+    wpa_supplicant \
+    wpa_supplicant.conf
 
 # inherit proprietary files
+$(call inherit-product, build/target/product/full_base.mk)
 $(call inherit-product-if-exists, vendor/acer/a200/a200-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, build/target/product/full_base.mk)
 $(call inherit-product-if-exists, framework/base/data/videos/VideoPackage2.mk)
 
-
-WIFI_BAND := 802_11_ABG
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
 
 PRODUCT_PACKAGES += \
@@ -46,15 +66,14 @@ PRODUCT_PACKAGES += \
     l2ping \
     hciconfig \
     hcitool \
-    libnetcmdiface \
     librs_jni \
     setup_fs \
     liba2dp \
     tinymix \
     tinyplay \
     tinycap \
-    tinyrec \
-    Torch 
+    tinyrec 
+
 
 # ramdisk
 PRODUCT_COPY_FILES += \
@@ -102,11 +121,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/etc/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Fix Wifi problems
-# $(LOCAL_PATH)/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf 
+# $(LOCAL_PATH)/prebuilt/bin/wpa_supplicant:system/bin/wpa_supplicant \
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/bin/wpa_supplicant:system/bin/wpa_supplicant \
-    $(LOCAL_PATH)/prebuilt/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
-    $(LOCAL_PATH)/prebuilt/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf 
+   $(LOCAL_PATH)/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf 
+  
+#    $(LOCAL_PATH)/prebuilt/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+#    $(LOCAL_PATH)/prebuilt/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf 
 
 # keychars/layout
 PRODUCT_COPY_FILES += \
@@ -134,17 +155,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal mdpi hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 
-PRODUCT_PROPERTY_OVERRIDES := \
-    wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=30 \
-    ro.dinfo.radio=Wifi \
-    ro.bq.gpu_to_cpu_unsupported=1 \
-    ro.wifi.country=EU \
-    ro.sys.fw.bg_apps_limit = 20 \
-    debug.hwui.render_dirty_regions=false \
-    ro.opengles.version=131072 \
-	ro.opengles.surface.rgb565=true \
-	ro.hwui.use_gpu_pixel_buffers=false
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
